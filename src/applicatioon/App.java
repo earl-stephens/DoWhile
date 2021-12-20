ce7559107b9cda0b6292c706773cd9062a9283e8
earl-stephens <stephensearl@hotmail.com>
2021-12-19 21:38:12 -0700

    Set up a do while loop

--- src/applicatioon/App.java
+++ src/applicatioon/App.java
@@ -3,8 +3,16 @@ package applicatioon;
 public class App {
 
 	public static void main(String[] args) {
-		
+		// Do while executes at least once, even if the condition is false
 
+		int count = 0;
+
+		do {
+			System.out.println("Count" + count);
+
+			//count++;
+			// Can increment inside the conditional below
+		} while (count++ < 10);
 	}
 
 }
